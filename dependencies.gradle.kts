485178f20de9fd43b2facd5529dcb4bf131538ff
Ondrej Janus <janusonx@gmail.com>
2023-03-08 07:51:06 +0100

    Create project

---
+++ dependencies.gradle.kts
@@ -0,0 +1,15 @@
+import org.gradle.script.lang.kotlin.extra
+import org.gradle.script.lang.kotlin.getValue
+import org.gradle.script.lang.kotlin.setValue
+
+var projectConfiguration: ProjectConfiguration by extra
+
+val openapiGeneratorPlugin = "org.openapitools:openapi-generator-gradle-plugin:6.0.1"
+val nebulaLintPlugin = "com.netflix.nebula:gradle-lint-plugin:18.0.3"
+
+projectConfiguration = ProjectConfiguration(
+    BuildPlugins(
+        openapiGeneratorPlugin,
+        nebulaLintPlugin
+    )
+)
